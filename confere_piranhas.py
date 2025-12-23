d7b2b87bdd4c13ede79c66778503d553e481c7a1
Gui Campos <camposgui@gmail.com>
2025-12-23 13:14:33 -0300

    Muda nomes na aba do navegador

--- confere_piranhas.py
+++ confere_piranhas.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Bolão das piranhas",
+    page_icon="🎲",
+    layout="centered"
+)
+
 st.title("🎲 Bolão Piranhas Rycassss")
 
 # Entrada do resultado
